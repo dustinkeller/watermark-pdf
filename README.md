c15d2c52c85e7bfe9ab6b252568d01b5eb2f4475
dustinkeller <dustin.w.keller@gmail.com>
2024-02-06 14:51:59 -0500

    finished watermarking functionality and README

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # watermark-pdf
-A Python script to add a watermark to all pages on a pdf file.
+## Purpose
+This Python script makes it quick and easy to add a watermark to all pages on a pdf file. This can be useful for specifying that a document's contents are private, not final, etc.
+
+## Usage
+Open the terminal, navigate to the directory containing this script, and run
+```
+python3 watermark.py /your/watermark/YOUR-FILENAME1-HERE.pdf/ /your/document/YOUR-FILENAME2-HERE.pdf/
+```
+Note that the watermarked pdf file will be named `wtr-YOUR-FILENAME2-HERE.pdf` and will be saved to the same directory as the document pdf file.  
+
+## Dependencies
+- [PyPDF2](https://pypdf2.readthedocs.io/en/latest/)
---
+++ watermark.py
@@ -0,0 +1,30 @@
+from PyPDF2 import PdfFileReader, PdfFileWriter
+from os import path
+from sys import argv, exit
+
+try:
+    watermark = argv[1]
+    document = argv[2]
+except IndexError:
+    exit("Please provide your watermark pdf file followed by your document pdf file...")
+
+with open(document, 'rb') as file:
+    reader = PdfFileReader(file)
+    writer = PdfFileWriter()
+
+    for page in range(reader.getNumPages()):
+        content_page = reader.getPage(page)
+        mediabox = content_page.mediaBox
+
+        reader_stamp = PdfFileReader(watermark)
+        image_page = reader_stamp.getPage(0)
+
+        image_page.mergePage(content_page)
+        image_page.mediaBox = mediabox
+        writer.addPage(image_page)
+
+directory, file_name = path.split(document)
+with open(path.join(directory, f'wtr_{file_name}'), 'wb') as fp:
+    writer.write(fp)
+
+print("Sucessfully watermarked pdf document!")
